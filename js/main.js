93e8e35cb77178bf35c1a0047fc52783d0aa3b75
mcle0341 <mcle0341@algonquinlive.com>
2016-03-21 09:57:33 -0400

    changed css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-var 
+
+var $html = $('html');
+var $ball = $('.ball');
+var $picker = $('form');
+var $input = $('#colour-picker');
+
+$picker.on('change', function () {
+  $ball.css('background-color', $input.val());
+});
